973e9362642476ac3b99f51b571631a5f87b5db5
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-26 16:16:32 +0530

    calculating no.of times head and tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 echo "It displays winner of Heads or Tails"
 
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "Enter the number of coin flip: " noOfCounts
+headCounts=0
+tailCounts=0
+for((i=1;i<=noOfCounts; i++))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 0 ]
+	then
+		echo "Heads"
+		headCounts=$((headCounts+1))
+	else
+		echo "Tails"
+		tailCounts=$((tailCounts+1))
+
+	fi
+done
+echo "Head wins: "$headCounts
+echo "Tail wins: "$tailCounts
